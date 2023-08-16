5d0377cdaef1960fff91554283a0ce81b9d9ddaf
arnaucube <root@arnaucube.com>
2023-08-16 17:38:26 +0200

    Update FoldingScheme trait to take C1 & C2 as params
    Update FoldingScheme trait to take C1 & C2 as params which are used by
    the diverse folding schemes as a cycle of curves.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+
 use ark_ec::CurveGroup;
 use ark_ff::PrimeField;
 use ark_std::{fmt::Debug, rand::RngCore};
+use thiserror::Error;
 
 pub mod transcript;
 
-#[derive(Debug)]
-pub enum Error {}
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Relation not satisfied")]
+    NotSatisfied,
+}
 
 pub trait Transcript<F: PrimeField> {
     type TranscriptConfig: Debug;
@@ -17,7 +24,12 @@ pub trait Transcript<F: PrimeField> {
     fn get_challenges(&mut self, n: usize) -> Vec<F>;
 }
 
-pub trait FoldingScheme<C: CurveGroup>: Clone + Debug {
+/// FoldingScheme defines trait that is implemented by the diverse folding schemes. It is defined
+/// over a cycle of curves (C1, C2), where:
+/// - C1 is the main curve, which ScalarField we use as our F for al the field operations
+/// - C2 is the auxiliary curve, which we use for the commitments, whose BaseField (for point
+/// coordinates) are in the C1::ScalarField
+pub trait FoldingScheme<C1: CurveGroup, C2: CurveGroup>: Clone + Debug {
     // type PCS: PolynomialCommitmentScheme<C>; // maybe not needed, just PedersenCommitment
     type PreprocessorParam: Debug;
     type ProverParam: Debug;
@@ -40,7 +52,7 @@ pub trait FoldingScheme<C: CurveGroup>: Clone + Debug {
         pp: &Self::ProverParam,
         running_instance: &mut Self::CommittedInstanceWithWitness,
         incomming_instances: &[Self::FreshInstance],
-        transcript: &mut impl Transcript<C::ScalarField>,
+        transcript: &mut impl Transcript<C1::ScalarField>,
         rng: impl RngCore,
     ) -> Result<(), Error>;
 
@@ -48,7 +60,7 @@ pub trait FoldingScheme<C: CurveGroup>: Clone + Debug {
         vp: &Self::VerifierParam,
         running_instance: &mut Self::CommittedInstance,
         incomming_instances: &[Self::PublicInput],
-        transcript: &mut impl Transcript<C::ScalarField>,
+        transcript: &mut impl Transcript<C1::ScalarField>,
         rng: impl RngCore,
     ) -> Result<(), Error>;
 }

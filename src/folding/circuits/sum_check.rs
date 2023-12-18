c60f21c03500813b10d9e25e25d7a13b26d3bc67
dmpierre <pdaixmoreux@gmail.com>
2023-12-18 10:18:40 +0100

    refactor: change naming to use the `Gadget` suffix, update `verify_sumcheck` to not have `&self` as first argument, update test

--- src/folding/circuits/sum_check.rs
+++ src/folding/circuits/sum_check.rs
@@ -1,3 +1,9 @@
+use crate::transcript::{poseidon::PoseidonTranscriptVar, TranscriptVar};
+/// Heavily inspired from testudo: https://github.com/cryptonetlab/testudo/tree/master
+/// Some changes:
+/// - Typings to better stick to ark_poly's API
+/// - Uses `folding-schemes`' own `TranscriptVar` trait and `PoseidonTranscriptVar` struct
+/// - API made closer to gadgets found in `folding-schemes`
 use ark_ff::PrimeField;
 use ark_poly::univariate::DensePolynomial;
 use ark_r1cs_std::{
@@ -6,14 +12,7 @@ use ark_r1cs_std::{
     fields::fp::FpVar,
 };
 use ark_relations::r1cs::{Namespace, SynthesisError};
-use std::borrow::Borrow;
-
-use crate::transcript::{poseidon::PoseidonTranscriptVar, TranscriptVar};
-use ark_r1cs_std::R1CSVar;
-
-/// Heavily inspired from testudo: https://github.com/cryptonetlab/testudo/tree/master
-/// Mostly changed typing to better stick to ark_poly's API
-/// Uses `folding-schemes`' own `TranscriptVar` trait and `PoseidonTranscriptVar` struct
+use std::{borrow::Borrow, marker::PhantomData};
 
 #[derive(Clone, Debug)]
 pub struct DensePolynomialVar<F: PrimeField> {
@@ -64,14 +63,13 @@ impl<F: PrimeField> DensePolynomialVar<F> {
     }
 }
 
-#[derive(Clone)]
-pub struct SumcheckVerificationCircuit<F: PrimeField> {
-    pub prover_polys: Vec<DensePolynomial<F>>,
+#[derive(Debug, Clone)]
+pub struct SumCheckVerifierGadget<F: PrimeField> {
+    _f: PhantomData<F>,
 }
 
-impl<F: PrimeField> SumcheckVerificationCircuit<F> {
+impl<F: PrimeField> SumCheckVerifierGadget<F> {
     pub fn verify_sumcheck(
-        &self,
         poly_vars: &[DensePolynomialVar<F>],
         claim_var: &FpVar<F>,
         transcript_var: &mut PoseidonTranscriptVar<F>,
@@ -81,7 +79,6 @@ impl<F: PrimeField> SumcheckVerificationCircuit<F> {
 
         for poly_var in poly_vars.iter() {
             let res = poly_var.eval_at_one() + poly_var.eval_at_zero();
-            println!("res: {:?}, e: {:?}", res.value(), e_var.value());
             res.enforce_equal(&e_var)?;
             transcript_var.absorb_vec(&poly_var.coeffs)?;
             let r_i_var = transcript_var.get_challenge()?;
@@ -112,10 +109,10 @@ mod tests {
     use ark_r1cs_std::fields::fp::FpVar;
     use ark_relations::r1cs::ConstraintSystem;
 
-    use super::{DensePolynomialVar, SumcheckVerificationCircuit};
+    use super::{DensePolynomialVar, SumCheckVerifierGadget};
 
     #[test]
-    fn sum_check_circuit() {
+    fn test_sum_check_circuit() {
         let poseidon_config = poseidon_test_config::<Fr>();
         let mut poseidon_transcript_prove: PoseidonTranscript<Projective> =
             PoseidonTranscript::<Projective>::new(&poseidon_config);
@@ -133,31 +130,26 @@ mod tests {
             .unwrap();
 
         // initiate univariate polynomial variables
-        let sumcheck_verification_circuit = SumcheckVerificationCircuit {
-            prover_polys: sum_check
-                .proofs
-                .iter()
-                .map(|message| DensePolynomial::from_coefficients_slice(&message.coeffs))
-                .collect(),
-        };
+        let mut poly_vars = Vec::with_capacity(sum_check.proofs.len());
+        sum_check.proofs.iter().for_each(|message| {
+            let poly_received = DensePolynomial::from_coefficients_slice(&message.coeffs);
+            let poly_received_var = DensePolynomialVar::new_variable(
+                cs.clone(),
+                || Ok(poly_received),
+                AllocationMode::Witness,
+            )
+            .unwrap();
+            poly_vars.push(poly_received_var);
+        });
 
-        let mut poly_vars = Vec::with_capacity(sumcheck_verification_circuit.prover_polys.len());
-        for poly in &sumcheck_verification_circuit.prover_polys {
-            let poly_var =
-                DensePolynomialVar::new_variable(cs.clone(), || Ok(poly), AllocationMode::Witness)
-                    .unwrap();
-            poly_vars.push(poly_var);
-        }
-
-        // initiate poseidon hash variable
-        let poly_num_variable = FpVar::new_variable(
+        let poly_num_variables_var = FpVar::new_variable(
             cs.clone(),
             || Ok(Fr::from(virtual_poly.aux_info.num_variables as u64)),
             AllocationMode::Witness,
         )
         .unwrap();
 
-        let poly_max_degree = FpVar::new_variable(
+        let poly_max_degree_var = FpVar::new_variable(
             cs.clone(),
             || Ok(Fr::from(virtual_poly.aux_info.max_degree as u64)),
             AllocationMode::Witness,
@@ -165,19 +157,16 @@ mod tests {
         .unwrap();
 
         let mut poseidon_var = PoseidonTranscriptVar::new(cs.clone(), &poseidon_config);
-        poseidon_var.absorb(poly_num_variable).unwrap();
-        poseidon_var.absorb(poly_max_degree).unwrap();
+        poseidon_var.absorb(poly_num_variables_var).unwrap();
+        poseidon_var.absorb(poly_max_degree_var).unwrap();
 
         let claim =
             IOPSumCheck::<Projective, PoseidonTranscript<Projective>>::extract_sum(&sum_check);
         let claim_var =
             FpVar::new_variable(cs.clone(), || Ok(claim), AllocationMode::Witness).unwrap();
 
-        let res = sumcheck_verification_circuit.verify_sumcheck(
-            &poly_vars,
-            &claim_var,
-            &mut poseidon_var,
-        );
+        let res =
+            SumCheckVerifierGadget::verify_sumcheck(&poly_vars, &claim_var, &mut poseidon_var);
 
         assert!(res.is_ok());
         assert!(cs.is_satisfied().unwrap());

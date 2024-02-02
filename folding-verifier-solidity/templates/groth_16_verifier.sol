05b539ff4671d5d04ae351e229e8471a7000348f
CPerezz <c.perezbaro@gmail.com>
2024-02-02 11:07:37 +0100

    change: Swap order of G2 coordinates within the template

--- folding-verifier-solidity/templates/groth_16_verifier.sol
+++ folding-verifier-solidity/templates/groth_16_verifier.sol
@@ -184,22 +184,22 @@ contract Verifier {
         );
 
         vk.beta2 = Pairing.G2Point(
-            [ {{ vkey_beta_g2.0[0][0] }},
-              {{ vkey_beta_g2.0[0][1] }}],
-            [ {{ vkey_beta_g2.0[1][0] }},
-              {{ vkey_beta_g2.0[1][1] }}]
+            [ {{ vkey_beta_g2.0[0][1] }},
+              {{ vkey_beta_g2.0[0][0] }}],
+            [ {{ vkey_beta_g2.0[1][1] }},
+              {{ vkey_beta_g2.0[1][0] }}]
         );
         vk.gamma2 = Pairing.G2Point(
-            [ {{ vkey_gamma_g2.0[0][0] }},
-              {{ vkey_gamma_g2.0[0][1] }}],
-            [ {{ vkey_gamma_g2.0[1][0] }},
-              {{ vkey_gamma_g2.0[1][1] }}]
+            [ {{ vkey_gamma_g2.0[0][1] }},
+              {{ vkey_gamma_g2.0[0][0] }}],
+            [ {{ vkey_gamma_g2.0[1][1] }},
+              {{ vkey_gamma_g2.0[1][0] }}]
         );
         vk.delta2 = Pairing.G2Point(
-            [ {{ vkey_delta_g2.0[0][0] }},
-              {{ vkey_delta_g2.0[0][1] }}],
-            [ {{ vkey_delta_g2.0[1][0] }},
-              {{ vkey_delta_g2.0[1][1] }}]
+            [ {{ vkey_delta_g2.0[0][1] }},
+              {{ vkey_delta_g2.0[0][0] }}],
+            [ {{ vkey_delta_g2.0[1][1] }},
+              {{ vkey_delta_g2.0[1][0] }}]
         );
         vk.IC = new Pairing.G1Point[]({{ gamma_abc_len }});
         {% for (i, point) in gamma_abc_g1.iter().enumerate() %}
@@ -237,7 +237,7 @@ contract Verifier {
         ) public view returns (bool r) {
         Proof memory proof;
         proof.A = Pairing.G1Point(a[0], a[1]);
-        proof.B = Pairing.G2Point([b[0][0], b[0][1]], [b[1][0], b[1][1]]);
+        proof.B = Pairing.G2Point([b[0][1], b[0][0]], [b[1][1], b[1][0]]);
         proof.C = Pairing.G1Point(c[0], c[1]);
         uint[] memory inputValues = new uint[](input.length);
         for(uint i = 0; i < input.length; i++){

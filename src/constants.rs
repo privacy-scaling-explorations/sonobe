40703db285f3b2c0caaca9c26ef04efca42c19ab
dmpierre <pdaixmoreux@gmail.com>
2024-02-07 14:43:54 +0100

    chore: end merge

--- src/constants.rs
+++
@@ -1,5 +0,0 @@
-// used for the RO challenges.
-// From [Srinath Setty](https://microsoft.com/en-us/research/people/srinath/): In Nova, soundness
-// error ≤ 2/|S|, where S is the subset of the field F from which the challenges are drawn. In this
-// case, we keep the size of S close to 2^128.
-pub const N_BITS_RO: usize = 128;

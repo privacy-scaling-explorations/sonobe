2d618445436c18b4681ae8021bb20aa4b5b1b68a
dmpierre <pdaixmoreux@gmail.com>
2024-02-07 17:10:14 +0100

    fix: update path to test circuit and add step for installing solc

--- folding-schemes/src/frontend/circom/test_folder/compile.sh
+++ folding-schemes/src/frontend/circom/test_folder/compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-circom ./src/frontend/circom/test_folder/test_circuit.circom --r1cs --wasm --prime bn128 --output ./src/frontend/circom/test_folder/
+circom ./folding-schemes/src/frontend/circom/test_folder/test_circuit.circom --r1cs --wasm --prime bn128 --output ./folding-schemes/src/frontend/circom/test_folder/

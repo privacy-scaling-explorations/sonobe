a8931b1e4510c1d3e9f8f8c6447755cd6c24a709
CPerezz <c.perezbaro@gmail.com>
2024-02-20 09:58:52 +0100

    chore: Rename KZG+G16 template
    Now the template refers to Nova + Cyclefold and has a Warning attached to it

--- folding-schemes-solidity/templates/nova_cyclefold_decider_verifier.askama.sol
+++ folding-schemes-solidity/templates/nova_cyclefold_decider_verifier.askama.sol
@@ -8,6 +8,8 @@ pragma solidity ^0.8.4;
  * @author  PSE & 0xPARC
  * @title   NovaDecider contract, for verifying zk-snarks Nova IVC proofs.
  * @dev     This is an askama template. It will feature a snarkjs groth16 and a kzg10 verifier, from which this contract inherits.
+ *          WARNING: This contract is known to not be sound. It lacks checks to ensure that no soundness issues can happen.
+ *          but for now, it's good enough for testing and benchmarking.
  */
 contract NovaDecider is Groth16Verifier, KZG10Verifier {
     function verifyProof(uint[2] calldata _pA, uint[2][2] calldata _pB, uint[2] calldata _pC, uint[{{ gamma_abc_len - 1 }}] calldata _pubSignals, uint256[2] calldata c, uint256[2] calldata pi, uint256 x, uint256 y) public view returns (bool) {

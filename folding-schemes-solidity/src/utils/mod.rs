f4b0503231b2aec703094a0e89e0722037382f1c
CPerezz <c.perezbaro@gmail.com>
2024-02-23 11:07:27 +0100

    add: HeaderInclusion template
    When we use templates that are composed by others (as happens with
    `NovaCyclefold` one) we sadly see that the License and the `pragma`
    attributes are rendered once per sub-template.
    
    This generic structure solves this issue by being actually the only item
    rendered which has a sub-template the template we indeed want to render
    at the end.

--- folding-schemes-solidity/src/utils/mod.rs
+++ folding-schemes-solidity/src/utils/mod.rs
@@ -1,1 +1,24 @@
+use askama::Template;
+
 pub mod encoding;
+
+#[derive(Template, Default)]
+#[template(path = "header_template.askama.sol", ext = "sol")]
+pub(crate) struct HeaderInclusion<T: Template> {
+    /// SPDX-License-Identifier
+    pub sdpx: String,
+    /// The `pragma` statement.
+    pub pragma_version: String,
+    /// The template to render alongside the header.
+    pub template: T,
+}
+
+impl<T: Template> From<(String, String, T)> for HeaderInclusion<T> {
+    fn from(value: (String, String, T)) -> Self {
+        Self {
+            sdpx: value.0,
+            pragma_version: value.1,
+            template: value.2,
+        }
+    }
+}

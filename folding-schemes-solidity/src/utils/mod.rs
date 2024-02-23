3a965efa8ce4ec5f02ca8de966c27f354a021a08
CPerezz <c.perezbaro@gmail.com>
2024-02-23 15:55:48 +0100

    add: Small builder-pattern to construct HeaderInclusion Templates
    As mentioned in previous commits, the idea is that the header is set on
    an automatic wrapper template applied to the one that we actually want
    to render.
    
    This builder pattern makes it less complex to do such a thing. Specially
    avoiding unidiomatic `From` implementations.

--- folding-schemes-solidity/src/utils/mod.rs
+++ folding-schemes-solidity/src/utils/mod.rs
@@ -1,4 +1,8 @@
+use std::fmt::Display;
+
 use askama::Template;
+
+use crate::{GPL3_SDPX_IDENTIFIER, PRAGMA_GROTH16_VERIFIER};
 
 pub mod encoding;
 
@@ -19,6 +23,47 @@ impl<T: Template> From<(String, String, T)> for HeaderInclusion<T> {
             sdpx: value.0,
             pragma_version: value.1,
             template: value.2,
+        }
+    }
+}
+
+impl<T: Template + Default> HeaderInclusion<T> {
+    pub fn builder() -> HeaderInclusionBuilder<T> {
+        HeaderInclusionBuilder::default()
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct HeaderInclusionBuilder<T: Template + Default> {
+    /// SPDX-License-Identifier
+    sdpx: String,
+    /// The `pragma` statement.
+    pragma_version: String,
+    /// The template to render alongside the header.
+    template: T,
+}
+
+impl<T: Template + Default> HeaderInclusionBuilder<T> {
+    pub fn sdpx<S: Into<String>>(mut self, sdpx: S) -> Self {
+        self.sdpx = sdpx.into();
+        self
+    }
+
+    pub fn pragma_version<S: Into<String>>(mut self, pragma_version: S) -> Self {
+        self.pragma_version = pragma_version.into();
+        self
+    }
+
+    pub fn template(mut self, template: T) -> Self {
+        self.template = template;
+        self
+    }
+
+    pub fn build(self) -> HeaderInclusion<T> {
+        HeaderInclusion {
+            sdpx: self.sdpx,
+            pragma_version: self.pragma_version,
+            template: self.template,
         }
     }
 }

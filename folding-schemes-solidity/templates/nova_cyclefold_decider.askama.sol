18fd256c5e9ed76ab2ae03ed8cc3849aa6d14107
CPerezz <c.perezbaro@gmail.com>
2024-02-26 15:03:02 +0100

    feat: Merge G16 and KZG contract results in NovaCyclefold

--- folding-schemes-solidity/templates/nova_cyclefold_decider.askama.sol
+++ folding-schemes-solidity/templates/nova_cyclefold_decider.askama.sol
@@ -11,9 +11,14 @@
  */
 contract NovaDecider is Groth16Verifier, KZG10Verifier {
     function verifyProof(uint[2] calldata _pA, uint[2][2] calldata _pB, uint[2] calldata _pC, uint[1] calldata _pubSignals, uint256[2] calldata c, uint256[2] calldata pi, uint256 x, uint256 y) public view returns (bool) {
-        return super.check(c, pi, x, y);
+        bool success_kzg = super.check(c, pi, x, y);
+        require(success_kzg == true, "KZG  Failed");
+        
         // for now, we do not relate the Groth16 and KZG10 proofs
         // this will done in the future, by computing challenge points from the groth16 proof's data
-        // super.verifyProof(_pA, _pB, _pC, _pubSignals);
+        bool success_g16 =  super.verifyProof(_pA, _pB, _pC, _pubSignals);
+        require(success_g16 == true, "G16 Failed");
+
+        return(true);
     }   
 }

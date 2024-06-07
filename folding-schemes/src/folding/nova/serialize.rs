d29bf0f493159bde40f6c09cbcd8b3435dc96c32
dmpierre <pdaixmoreux@gmail.com>
2024-06-07 15:49:24 +0200

    feat: remove serializing r1cs and cs params and provide them at deserialization time

--- folding-schemes/src/folding/nova/serialize.rs
+++ folding-schemes/src/folding/nova/serialize.rs
@@ -1,17 +1,17 @@
 pub use super::{CommittedInstance, Witness};
 pub use crate::folding::circuits::CF2;
 use crate::{ccs::r1cs::R1CS, commitment::CommitmentScheme, frontend::FCircuit};
-use ark_crypto_primitives::sponge::poseidon::PoseidonConfig;
-use ark_ec::CurveGroup;
+use ark_crypto_primitives::sponge::{poseidon::PoseidonConfig, Absorb};
+use ark_ec::{CurveGroup, Group};
 use ark_ff::PrimeField;
-use ark_r1cs_std::groups::GroupOpsBounds;
-use ark_r1cs_std::{groups::CurveVar, ToConstraintFieldGadget};
+use ark_r1cs_std::{
+    groups::{CurveVar, GroupOpsBounds},
+    ToConstraintFieldGadget,
+};
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Write};
 use std::marker::PhantomData;
 
-use super::Nova;
-use ark_crypto_primitives::sponge::Absorb;
-use ark_ec::Group;
+use super::{Nova, ProverParams};
 
 impl<C1, GC1, C2, GC2, FC, CS1, CS2> CanonicalSerialize for Nova<C1, GC1, C2, GC2, FC, CS1, CS2>
 where
@@ -30,18 +30,12 @@ where
     GC1: CurveVar<C1, <C2 as Group>::ScalarField>,
     GC1: ToConstraintFieldGadget<<C2 as Group>::ScalarField>,
     GC2: CurveVar<C2, <C2 as CurveGroup>::BaseField>,
-    CS1::ProverParams: CanonicalSerialize,
-    CS2::ProverParams: CanonicalSerialize,
 {
     fn serialize_with_mode<W: Write>(
         &self,
         mut writer: W,
         compress: ark_serialize::Compress,
     ) -> Result<(), ark_serialize::SerializationError> {
-        // we serialize non-deterministic parameters
-        self.cs_params.serialize_with_mode(&mut writer, compress)?;
-        self.cf_cs_params
-            .serialize_with_mode(&mut writer, compress)?;
         self.i.serialize_with_mode(&mut writer, compress)?;
         self.z_0.serialize_with_mode(&mut writer, compress)?;
         self.z_i.serialize_with_mode(&mut writer, compress)?;
@@ -50,38 +44,11 @@ where
         self.W_i.serialize_with_mode(&mut writer, compress)?;
         self.U_i.serialize_with_mode(&mut writer, compress)?;
         self.cf_W_i.serialize_with_mode(&mut writer, compress)?;
-        self.cf_U_i.serialize_with_mode(&mut writer, compress)?;
-        self.r1cs.serialize_with_mode(&mut writer, compress)?;
-        self.cf_r1cs.serialize_with_mode(&mut writer, compress)?;
-        // we serialize PoseidonConfig without implementing the `CanonicalDeserialize` trait
-        // this is because we can not implement an external trait on an external type.
-        self.poseidon_config
-            .full_rounds
-            .serialize_with_mode(&mut writer, compress)?;
-        self.poseidon_config
-            .partial_rounds
-            .serialize_with_mode(&mut writer, compress)?;
-        self.poseidon_config
-            .alpha
-            .serialize_with_mode(&mut writer, compress)?;
-        self.poseidon_config
-            .ark
-            .serialize_with_mode(&mut writer, compress)?;
-        self.poseidon_config
-            .mds
-            .serialize_with_mode(&mut writer, compress)?;
-        self.poseidon_config
-            .rate
-            .serialize_with_mode(&mut writer, compress)?;
-        self.poseidon_config
-            .capacity
-            .serialize_with_mode(&mut writer, compress)
+        self.cf_U_i.serialize_with_mode(&mut writer, compress)
     }
 
     fn serialized_size(&self, compress: ark_serialize::Compress) -> usize {
-        self.cs_params.serialized_size(compress)
-            + self.cf_cs_params.serialized_size(compress)
-            + self.i.serialized_size(compress)
+        self.i.serialized_size(compress)
             + self.z_0.serialized_size(compress)
             + self.z_i.serialized_size(compress)
             + self.w_i.serialized_size(compress)
@@ -90,18 +57,6 @@ where
             + self.U_i.serialized_size(compress)
             + self.cf_W_i.serialized_size(compress)
             + self.cf_U_i.serialized_size(compress)
-            + self.r1cs.serialized_size(compress)
-            + self.cf_r1cs.serialized_size(compress)
-            + self.poseidon_config.full_rounds.serialized_size(compress)
-            + self
-                .poseidon_config
-                .partial_rounds
-                .serialized_size(compress)
-            + self.poseidon_config.alpha.serialized_size(compress)
-            + self.poseidon_config.ark.serialized_size(compress)
-            + self.poseidon_config.mds.serialized_size(compress)
-            + self.poseidon_config.rate.serialized_size(compress)
-            + self.poseidon_config.capacity.serialized_size(compress)
     }
 
     fn serialize_compressed<W: Write>(
@@ -146,17 +101,16 @@ where
     GC1: CurveVar<C1, <C2 as Group>::ScalarField>,
     GC1: ToConstraintFieldGadget<<C2 as Group>::ScalarField>,
     GC2: CurveVar<C2, <C2 as CurveGroup>::BaseField>,
-    CS1::ProverParams: CanonicalDeserialize,
-    CS2::ProverParams: CanonicalDeserialize,
 {
     pub fn deserialize_nova<R: std::io::prelude::Read>(
         mut reader: R,
         compress: ark_serialize::Compress,
         validate: ark_serialize::Validate,
+        r1cs: R1CS<C1::ScalarField>,
+        cf_r1cs: R1CS<C2::ScalarField>,
+        prover_params: ProverParams<C1, C2, CS1, CS2>,
+        poseidon_config: PoseidonConfig<C1::ScalarField>,
     ) -> Result<Self, ark_serialize::SerializationError> {
-        let cs_params = CS1::ProverParams::deserialize_with_mode(&mut reader, compress, validate)?;
-        let cf_cs_params =
-            CS2::ProverParams::deserialize_with_mode(&mut reader, compress, validate)?;
         let i = C1::ScalarField::deserialize_with_mode(&mut reader, compress, validate)?;
         let z_0 = Vec::<C1::ScalarField>::deserialize_with_mode(&mut reader, compress, validate)?;
         let z_i = Vec::<C1::ScalarField>::deserialize_with_mode(&mut reader, compress, validate)?;
@@ -167,36 +121,13 @@ where
         let cf_W_i = Witness::<C2>::deserialize_with_mode(&mut reader, compress, validate)?;
         let cf_U_i =
             CommittedInstance::<C2>::deserialize_with_mode(&mut reader, compress, validate)?;
-        let r1cs = R1CS::<C1::ScalarField>::deserialize_with_mode(&mut reader, compress, validate)?;
-        let cf_r1cs =
-            R1CS::<C2::ScalarField>::deserialize_with_mode(&mut reader, compress, validate)?;
-
-        let full_rounds = usize::deserialize_with_mode(&mut reader, compress, validate)?;
-        let partial_rounds = usize::deserialize_with_mode(&mut reader, compress, validate)?;
-        let alpha = u64::deserialize_with_mode(&mut reader, compress, validate)?;
-        let ark =
-            Vec::<Vec<C1::ScalarField>>::deserialize_with_mode(&mut reader, compress, validate)?;
-        let mds =
-            Vec::<Vec<C1::ScalarField>>::deserialize_with_mode(&mut reader, compress, validate)?;
-        let rate = usize::deserialize_with_mode(&mut reader, compress, validate)?;
-        let capacity = usize::deserialize_with_mode(&mut reader, compress, validate)?;
         let f_circuit = FC::new(()).unwrap();
-        let poseidon_config = PoseidonConfig {
-            full_rounds,
-            partial_rounds,
-            alpha,
-            ark,
-            mds,
-            rate,
-            capacity,
-        };
-
         Ok(Nova {
             _gc1: PhantomData,
             _c2: PhantomData,
             _gc2: PhantomData,
-            cs_params,
-            cf_cs_params,
+            cs_params: prover_params.cs_params,
+            cf_cs_params: prover_params.cf_cs_params,
             i,
             z_0,
             z_i,
@@ -281,17 +212,24 @@ pub mod tests {
 
         let bytes = fs::read("./nova.serde").unwrap();
 
-        let mut deserialized_nova =
-            Nova::<
-                Projective,
-                GVar,
-                Projective2,
-                GVar2,
-                CubicFCircuit<Fr>,
-                KZG<Bn254>,
-                Pedersen<Projective2>,
-            >::deserialize_nova(bytes.as_slice(), Compress::No, Validate::No)
-            .unwrap();
+        let mut deserialized_nova = Nova::<
+            Projective,
+            GVar,
+            Projective2,
+            GVar2,
+            CubicFCircuit<Fr>,
+            KZG<Bn254>,
+            Pedersen<Projective2>,
+        >::deserialize_nova(
+            bytes.as_slice(),
+            Compress::No,
+            Validate::No,
+            nova.r1cs.clone(),
+            nova.cf_r1cs.clone(),
+            prover_params,
+            poseidon_config,
+        )
+        .unwrap();
 
         assert_eq!(nova.i, deserialized_nova.i);
 

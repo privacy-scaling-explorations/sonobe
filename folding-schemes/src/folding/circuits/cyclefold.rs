privacy-scaling-explorations/folding-schemes

Rename `N_BITS_RO` to `NOVA_N_BITS_RO`

folding-schemes/src/folding/circuits/cyclefold.rs
@@ -22,7 +22,7 @@ use core::{borrow::Borrow, marker::PhantomData};
 use super::{nonnative::uint::NonNativeUintVar, CF2};
 use crate::ccs::r1cs::{extract_w_x, R1CS};
 use crate::commitment::CommitmentScheme;
-use crate::constants::N_BITS_RO;
+use crate::constants::NOVA_N_BITS_RO;
 use crate::folding::nova::{nifs::NIFS, CommittedInstance, Witness};
 use crate::frontend::FCircuit;
 use crate::transcript::{AbsorbNonNativeGadget, Transcript, TranscriptVar};
@@ -248,7 +248,7 @@ where
         transcript.absorb_nonnative(&U_i);
         transcript.absorb_nonnative(&u_i);
         transcript.absorb_point(&cmT);
-        transcript.squeeze_bits(N_BITS_RO)
+        transcript.squeeze_bits(NOVA_N_BITS_RO)
     }
 
     // compatible with the native get_challenge_native
@@ -261,7 +261,7 @@ where
         transcript.absorb(&U_i_vec)?;
         transcript.absorb_nonnative(&u_i)?;
         transcript.absorb_point(&cmT)?;
-        transcript.squeeze_bits(N_BITS_RO)
+        transcript.squeeze_bits(NOVA_N_BITS_RO)
     }
 }
 
@@ -442,7 +442,7 @@ pub mod tests {
     }
 
     impl<C: CurveGroup> CycleFoldConfig for TestCycleFoldConfig<C> {
-        const N_BITS_RO: usize = N_BITS_RO;
+        const N_BITS_RO: usize = NOVA_N_BITS_RO;
         type C = C;
         type F = C::BaseField;
     }

folding-schemes/src/folding/nova/mod.rs
@@ -26,7 +26,7 @@ use crate::Error;
 use crate::FoldingScheme;
 use crate::{
     ccs::r1cs::{extract_r1cs, extract_w_x, R1CS},
-    constants::N_BITS_RO,
+    constants::NOVA_N_BITS_RO,
 };
 
 pub mod circuits;
@@ -45,7 +45,7 @@ struct NovaCycleFoldConfig<C: CurveGroup> {
 }
 
 impl<C: CurveGroup> CycleFoldConfig for NovaCycleFoldConfig<C> {
-    const N_BITS_RO: usize = N_BITS_RO;
+    const N_BITS_RO: usize = NOVA_N_BITS_RO;
     type C = C;
     type F = C::BaseField;
 }

privacy-scaling-explorations/sonobe

folding-schemes/src/folding/circuits/cyclefold.rs
@@ -23,7 +23,7 @@ use core::{borrow::Borrow, marker::PhantomData};
 use super::{nonnative::uint::NonNativeUintVar, CF2};
 use crate::arith::r1cs::{extract_w_x, R1CS};
 use crate::commitment::CommitmentScheme;
-use crate::constants::N_BITS_RO;
+use crate::constants::NOVA_N_BITS_RO;
 use crate::folding::nova::{nifs::NIFS, CommittedInstance, Witness};
 use crate::transcript::{AbsorbNonNativeGadget, Transcript, TranscriptVar};
 use crate::Error;
@@ -252,7 +252,7 @@ where
         transcript.absorb_nonnative(&U_i);
         transcript.absorb_nonnative(&u_i);
         transcript.absorb_point(&cmT);
-        transcript.squeeze_bits(N_BITS_RO)
+        transcript.squeeze_bits(NOVA_N_BITS_RO)
     }
 
     // compatible with the native get_challenge_native
@@ -267,7 +267,7 @@ where
         transcript.absorb(&U_i_vec)?;
         transcript.absorb_nonnative(&u_i)?;
         transcript.absorb_point(&cmT)?;
-        transcript.squeeze_bits(N_BITS_RO)
+        transcript.squeeze_bits(NOVA_N_BITS_RO)
     }
 }
 
@@ -505,7 +505,7 @@ pub mod tests {
     }
 
     impl<C: CurveGroup> CycleFoldConfig for TestCycleFoldConfig<C> {
-        const RANDOMNESS_BIT_LENGTH: usize = N_BITS_RO;
+        const RANDOMNESS_BIT_LENGTH: usize = NOVA_N_BITS_RO;
         const N_INPUT_POINTS: usize = 2;
         type C = C;
         type F = C::BaseField;

folding-schemes/src/folding/nova/mod.rs
@@ -26,7 +26,7 @@ use crate::Error;
 use crate::FoldingScheme;
 use crate::{
     arith::r1cs::{extract_r1cs, extract_w_x, R1CS},
-    constants::N_BITS_RO,
+    constants::NOVA_N_BITS_RO,
     utils::{get_cm_coordinates, pp_hash},
 };
 
@@ -46,7 +46,7 @@ struct NovaCycleFoldConfig<C: CurveGroup> {
 }
 
 impl<C: CurveGroup> CycleFoldConfig for NovaCycleFoldConfig<C> {
-    const RANDOMNESS_BIT_LENGTH: usize = N_BITS_RO;
+    const RANDOMNESS_BIT_LENGTH: usize = NOVA_N_BITS_RO;
     const N_INPUT_POINTS: usize = 2;
     type C = C;
     type F = C::BaseField;
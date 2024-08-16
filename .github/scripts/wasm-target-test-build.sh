9e214d6bd4886e32b5e92693beb0830ef8bba4d9
CPerezz <c.perezbaro@gmail.com>
2024-08-16 16:21:53 +0200

    fix: Update CI to use correct feature sets

--- .github/scripts/wasm-target-test-build.sh
+++ .github/scripts/wasm-target-test-build.sh
@@ -12,7 +12,7 @@ cd foobar
 cp "${GIT_ROOT}/rust-toolchain" .
 
 # add wasm32-* targets
-rustup target add wasm32-unknown-unknown wasm32-wasi wasm32-unknown-emscripten
+rustup target add wasm32-unknown-unknown wasm32-wasi 
 
 # add dependencies
 cargo add --path "${GIT_ROOT}/folding-schemes" --features wasm, parallel
@@ -21,7 +21,8 @@ cargo add getrandom --features js --target wasm32-unknown-unknown
 # test build for wasm32-* targets
 cargo build --release --target wasm32-unknown-unknown
 cargo build --release --target wasm32-wasi
-cargo build --release --target wasm32-unknown-emscripten
+# Emscripten would require to fetch the `emcc` tooling. Hence we don't build the lib as a dep for it.
+# cargo build --release --target wasm32-unknown-emscripten
 
 # delete test project
 cd ../

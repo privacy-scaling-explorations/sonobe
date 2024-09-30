4e5fec89d8d78fa97e92fe893f00b5654ed687fe
winderica <winderica@gmail.com>
2024-09-30 15:28:44 +0800

    Structs for proofs and auxiliary values in protogalaxy

--- folding-schemes/src/folding/protogalaxy/mod.rs
+++ folding-schemes/src/folding/protogalaxy/mod.rs
@@ -717,7 +717,7 @@ where
         } else {
             // Primary part:
             // Compute `U_{i+1}` by folding `u_i` into `U_i`.
-            let (U_i1, W_i1, F_coeffs, K_coeffs, L_evals, phi_stars) = Folding::prove(
+            let (U_i1, W_i1, proof, aux) = Folding::prove(
                 &mut transcript_prover,
                 &self.r1cs,
                 &self.U_i,
@@ -728,8 +728,8 @@ where
 
             // CycleFold part:
             // get the vector used as public inputs 'x' in the CycleFold circuit
-            let mut r0_bits = L_evals[0].into_bigint().to_bits_le();
-            let mut r1_bits = L_evals[1].into_bigint().to_bits_le();
+            let mut r0_bits = aux.L_X_evals[0].into_bigint().to_bits_le();
+            let mut r1_bits = aux.L_X_evals[1].into_bigint().to_bits_le();
             r0_bits.resize(C1::ScalarField::MODULUS_BIT_SIZE as usize, false);
             r1_bits.resize(C1::ScalarField::MODULUS_BIT_SIZE as usize, false);
 
@@ -744,7 +744,7 @@ where
                     .unwrap(),
                 get_cm_coordinates(&C1::zero()),
                 get_cm_coordinates(&self.U_i.phi),
-                get_cm_coordinates(&phi_stars[0]),
+                get_cm_coordinates(&aux.phi_stars[0]),
             ]
             .concat();
             let cf1_circuit = ProtoGalaxyCycleFoldCircuit::<C1, GC1> {
@@ -764,7 +764,7 @@ where
                     .map(C1::BaseField::from_bigint)
                     .collect::<Option<Vec<_>>>()
                     .unwrap(),
-                get_cm_coordinates(&phi_stars[0]),
+                get_cm_coordinates(&aux.phi_stars[0]),
                 get_cm_coordinates(&self.u_i.phi),
                 get_cm_coordinates(&U_i1.phi),
             ]
@@ -772,7 +772,7 @@ where
             let cf2_circuit = ProtoGalaxyCycleFoldCircuit::<C1, GC1> {
                 _gc: PhantomData,
                 r_bits: Some(r1_bits),
-                points: Some(vec![phi_stars[0], self.u_i.phi]),
+                points: Some(vec![aux.phi_stars[0], self.u_i.phi]),
                 x: Some(cf2_u_i_x.clone()),
             };
 
@@ -817,9 +817,9 @@ where
                 u_i_phi: self.u_i.phi,
                 U_i: self.U_i.clone(),
                 U_i1_phi: U_i1.phi,
-                F_coeffs: F_coeffs.clone(),
-                K_coeffs: K_coeffs.clone(),
-                phi_stars,
+                F_coeffs: proof.F_coeffs.clone(),
+                K_coeffs: proof.K_coeffs.clone(),
+                phi_stars: aux.phi_stars,
                 F: self.F.clone(),
                 x: Some(u_i1_x),
                 // cyclefold values
@@ -839,8 +839,7 @@ where
                         &mut transcript_verifier,
                         &self.U_i,
                         &[self.u_i.clone()],
-                        F_coeffs,
-                        K_coeffs
+                        proof
                     )?,
                     U_i1
                 );
